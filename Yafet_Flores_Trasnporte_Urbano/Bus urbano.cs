025f9595fef3a9fe793b30a90f4fa514789594df
YafetFz <ayafetfz@gmail.com>
2026-02-14 20:34:10 -0600

    Se crearo las clases de Bus, Taxi y Bici

---
+++ Yafet_Flores_Trasnporte_Urbano/Bus urbano.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yafet_Flores_Trasnporte_Urbano
+{
+    public class Bus_urbano
+    {
+
+    }
+
+
+}
---
+++ Yafet_Flores_Trasnporte_Urbano/Taxi.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yafet_Flores_Trasnporte_Urbano
+{
+    internal class Taxi
+    {
+    }
+}
